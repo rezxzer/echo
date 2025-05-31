3b419c9948d1e770cc5201e43e17b7e6a718aa95
rezxzer <rezorezorezo123321@gmail.com>
2025-05-31 13:44:07 +0400

    Update Supabase URL

--- js/supabase-config.js
+++ js/supabase-config.js
@@ -1,5 +1,5 @@
 // Supabase configuration
-const SUPABASE_URL = 'https://rezxzer.supabase.co';
+const SUPABASE_URL = 'https://utruirfngebwhqkfcecz.supabase.co';
 const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJlenh6ZXIiLCJyb2xlIjoiYW5vbiIsImlhdCI6MTcxMTg5NjAwMCwiZXhwIjoyMDI3NDcyMDAwfQ.YourKeyHere';
 
 // Initialize Supabase client
